cd798c667442df44377cc3340ee04168e7ed7dac
ikhdaakmalia <punyaamelaja@gmail.com>
2024-11-22 11:14:50 +0700

    membuat file SplashView

---
+++ app/src/main/java/com/example/privateclass/ui/view/screen/SplashView.kt
@@ -0,0 +1,12 @@
+package com.example.privateclass.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview(showBackground = true)
+@Composable
+
+fun SplashView(
+
+){}
