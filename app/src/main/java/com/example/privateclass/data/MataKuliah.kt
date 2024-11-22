be50d17b8bf32d237a005dcd019d42d287a654fe
ikhdaakmalia <punyaamelaja@gmail.com>
2024-11-22 11:36:51 +0700

    membuat object MataKuliah dengan isi berbagai jenis mata kuliah

---
+++ app/src/main/java/com/example/privateclass/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.privateclass.data
+
+object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototypeing",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
+}
