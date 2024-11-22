175b3da296148392d01d29d2c63f9f45255a8c53
ikhdaakmalia <punyaamelaja@gmail.com>
2024-11-22 12:14:52 +0700

    membuat fungsi DetailView untuk menampilkan seluh data inputan

---
+++ app/src/main/java/com/example/privateclass/ui/view/screen/DetailView.kt
@@ -0,0 +1,30 @@
+package com.example.privateclass.ui.view.screen
+
+import androidx.compose.foundation.content.MediaType
+import androidx.compose.foundation.content.MediaType.Companion.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.privateclass.model.Mahasiswa
+import com.example.privateclass.model.RencanaStudi
+
+@Composable
+fun DatailView(
+    mahasiswa: Mahasiswa,
+    rencanaStudi: RencanaStudi,
+    onBackButtonClicked: () -> Unit,
+    onResetButtonClicked: () -> Unit
+) {
+
+}
